221860f7566c227df06959e76fe7e4a229cd653d
JacketOfSeville <jacketfollowme@gmail.com>
2024-07-03 00:14:07 -0300

    feat: Exercício 2

--- src/env/tools/AuctionArt.java
+++ src/env/tools/AuctionArt.java
@@ -16,16 +16,28 @@ public class AuctionArt extends Artifact {
         defineObsProperty("maxValue",      maxValue);
         defineObsProperty("currentBid",    maxValue);
         defineObsProperty("currentWinner", "no_winner");
+        defineObsProperty("auctionState", "open"); // Novo: nova propriedade observável para estado do leilão
     }
 
     @OPERATION public void bid(double bidValue) {
         ObsProperty opCurrentValue  = getObsProperty("currentBid");
         ObsProperty opCurrentWinner = getObsProperty("currentWinner");
-        if (bidValue < opCurrentValue.doubleValue()) {  // o lance é melhor que o anterior
-            opCurrentValue.updateValue(bidValue);
-            opCurrentWinner.updateValue(getCurrentOpAgentId().getAgentName());
+        ObsProperty opAuctionState = getObsProperty("auctionState");
+
+        if (opAuctionState.stringValue().equals("open")) { // Novo: verifica se o leilão está aberto
+            if (bidValue < opCurrentValue.doubleValue()) {  // o lance é melhor que o anterior
+                opCurrentValue.updateValue(bidValue);
+                opCurrentWinner.updateValue(getCurrentOpAgentId().getAgentName());
+            }
+        } else { // Novo:
+            // Leilão fechado, lance não permitido
+            throw new RuntimeException("Auction closed");
         }
     }
 
+    @OPERATION public void clearAuction() { // Novo: Fecha o leilão
+        ObsProperty opAuctionState = getObsProperty("auctionState");
+        opAuctionState.updateValue("closed");
+    }
 }
 
